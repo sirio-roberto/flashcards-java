56aa4d8445cc913f78a40dfc4e01c8c3d22b27da
Sirio Junior <sirioroberto1@hotmail.com>
2023-03-12 09:24:49 -0300

    replaced hashMap with Card class so we can implement the mistakes properly

--- flashcards/Card.java
+++ flashcards/Card.java
@@ -1,12 +1,23 @@
 package flashcards;
 
+import java.util.Objects;
+
 public class Card {
     private String term;
     private String definition;
 
+    private int mistakes;
+
     public Card(String term, String definition) {
         this.term = term;
         this.definition = definition;
+        this.mistakes = 0;
+    }
+
+    public Card(String term, String definition, int mistakes) {
+        this.term = term;
+        this.definition = definition;
+        this.mistakes = mistakes;
     }
 
     public String getTerm() {
@@ -23,5 +34,30 @@ public class Card {
 
     public void setDefinition(String definition) {
         this.definition = definition;
+    }
+
+    public int getMistakes() {
+        return mistakes;
+    }
+
+    public void setMistakes(int mistakes) {
+        this.mistakes = mistakes;
+    }
+
+    public void addMistake() {
+        this.mistakes++;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Card card = (Card) o;
+        return Objects.equals(term, card.term) && Objects.equals(definition, card.definition);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(term, definition);
     }
 }
--- flashcards/Main.java
+++ flashcards/Main.java
@@ -6,7 +6,7 @@ import java.util.*;
 public class Main {
     static Scanner scan = new Scanner(System.in);
 
-    static HashMap<String, String> cards = new HashMap<>();
+    static HashSet<Card> cards = new HashSet<>();
 
     public static void main(String[] args) {
         startProgram();
@@ -15,7 +15,7 @@ public class Main {
     private static void startProgram() {
         String userAction;
         do {
-            System.out.println("Input the action (add, remove, import, export, ask, exit):");
+            System.out.println("Input the action (add, remove, import, export, ask, exit, log, hardest card, reset stats):");
             userAction = scan.nextLine();
             switch (userAction) {
                 case "add" -> addCard();
@@ -33,21 +33,21 @@ public class Main {
     private static void askQuestions() {
         System.out.println("How many times to ask?");
         int quantityOfQuestions = Integer.parseInt(scan.nextLine());
-        List<String> keyList = new ArrayList<>(cards.keySet());
+        List<Card> cardList = new ArrayList<>(cards);
         for (int i = 0; i < quantityOfQuestions; i++) {
-            int listIndex = new Random().nextInt(keyList.size());
+            int listIndex = new Random().nextInt(cardList.size());
 
-            String termToAsk = keyList.get(listIndex);
+            String termToAsk = cardList.get(listIndex).getTerm();
             System.out.printf("Print the definition of \"%s\":%n", termToAsk);
             String userDefinition = scan.nextLine();
 
-            if (Objects.equals(cards.get(termToAsk), userDefinition)) {
+            if (Objects.equals(cardList.get(listIndex).getDefinition(), userDefinition)) {
                 System.out.println("Correct!");
             } else {
                 StringBuilder wrongMessage = new StringBuilder("Wrong. The right answer is \"");
-                wrongMessage.append(cards.get(termToAsk)).append("\"");
+                wrongMessage.append(cardList.get(listIndex).getDefinition()).append("\"");
 
-                if (cards.containsValue(userDefinition)) {
+                if (hasDefinition(userDefinition)) {
                     wrongMessage.append(", but your definition is correct for \"");
                     wrongMessage.append(getTermFromDefinition(userDefinition)).append("\".");
                 } else {
@@ -59,9 +59,9 @@ public class Main {
     }
 
     private static String getTermFromDefinition(String userDefinition) {
-        for (String term: cards.keySet()) {
-            if (cards.get(term).equals(userDefinition)) {
-                return term;
+        for (Card card: cards) {
+            if (card.getDefinition().equals(userDefinition)) {
+                return card.getTerm();
             }
         }
         return "";
@@ -71,10 +71,10 @@ public class Main {
         System.out.println("File name:");
         String exportPath = scan.nextLine();
         try (BufferedWriter bw = new BufferedWriter(new FileWriter(exportPath))) {
-            for (String key: cards.keySet()) {
-                bw.write(key);
+            for (Card card: cards) {
+                bw.write(card.getTerm());
                 bw.write(",");
-                bw.write(cards.get(key));
+                bw.write(card.getDefinition());
                 bw.newLine();
             }
             System.out.printf("%s cards have been saved.%n", cards.size());
@@ -97,7 +97,7 @@ public class Main {
         String[] lines = bf.lines().toArray(String[]::new);
         for (String line: lines) {
             String[] fields = line.split(",");
-            cards.put(fields[0], fields[1]);
+            cards.add(new Card(fields[0], fields[1]));
         }
         return lines.length;
     }
@@ -105,8 +105,8 @@ public class Main {
     private static void removeCard() {
         System.out.println("Which card?");
         String cardToDelete = scan.nextLine();
-        if (cards.containsKey(cardToDelete)) {
-            cards.remove(cardToDelete);
+        if (hasTerm(cardToDelete)) {
+            cards.removeIf(card -> card.getTerm().equals(cardToDelete));
             System.out.println("The card has been removed.");
         } else {
             System.out.printf("Can't remove \"%s\": there is no such card.%n", cardToDelete);
@@ -116,17 +116,35 @@ public class Main {
     private static void addCard() {
         System.out.println("The card:");
         String term = scan.nextLine();
-        if (cards.containsKey(term)) {
+        if (hasTerm(term)) {
             System.out.printf("The card \"%s\" already exists.%n", term);
         } else {
             System.out.println("The definition of the card:");
             String definition = scan.nextLine();
-            if (cards.containsValue(definition)) {
+            if (hasDefinition(definition)) {
                 System.out.printf("The definition \"%s\" already exists.%n", definition);
             } else {
-                cards.put(term, definition);
+                cards.add(new Card(term, definition));
                 System.out.printf("The pair (\"%s\":\"%s\") has been added.%n", term, definition);
             }
         }
+    }
+
+    private static boolean hasTerm(String term) {
+        for (Card card: cards) {
+            if (card.getTerm().equals(term)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean hasDefinition(String definition) {
+        for (Card card: cards) {
+            if (card.getDefinition().equals(definition)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
